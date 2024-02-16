503435a21d1ff81cdcc72fb2768e4aa7f54cde92
Avinash Sharma <42712946+avinash-create@users.noreply.github.com>
2024-02-16 12:22:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ const codeInputs = document.querySelectorAll('.code');
+    
+    codeInputs.forEach((input, index) => {
+      input.addEventListener('input', (event) => {
+        if (event.target.value.length === 1 && index < codeInputs.length - 1) {
+          codeInputs[index + 1].focus();
+        }
+      });
+      
+      input.addEventListener('keydown', (event) => {
+        if (event.key === 'Backspace' && index > 0) {
+          codeInputs[index - 1].focus();
+        }
+      });
+    });
